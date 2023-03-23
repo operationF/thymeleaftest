77d3cf8c1004ce806bf63272db419b45e4bcb326
Lee HyunJy <lhj980424@gmail.com>
2023-03-23 21:44:46 +0900

    feat : add validator

--- src/main/java/com/example/thymeleaftest/model/Board.java
+++ src/main/java/com/example/thymeleaftest/model/Board.java
@@ -4,7 +4,9 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
+import jakarta.validation.constraints.NotNull;
 
 @Data
 @Entity
@@ -12,6 +14,9 @@ public class Board {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotNull
+    @Size(min=2, max=30, message = "제목은 2자 이상 30자 이하입니다.")
     private String title;
+
     private String content;
 }
